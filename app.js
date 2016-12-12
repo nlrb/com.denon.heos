85b6be43be3d049f966e9d3cbeea51263fd92761
Emile Nijssen <emile@athom.nl>
2016-12-12 20:52:58 +0100

    working

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
 'use strict';
 
-var Heos = require('heos');
-
-Heos.connect()
-    .then(function(heos) {
-
-        // Get all your players (speakers or groups), this takes some seconds
-        return heos.getPlayers();
-
-    })
-    .then(function(players) {
-
-        console.log('Found players', players);
-
-        // Pick the first and get what it is playing
-        return players[0].getNowPlaying()
-            .then(function(media) {
-                console.log('Playing:', media);
-            });
-
-    })
-    .catch(function(err) {
-        console.log('Error', err);
-    });
-
-setInterval(function(){}, 1000)
---
+++ drivers/denonheos/driver.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const Driver = require('../../lib/Driver.js');
+
+module.exports = new Driver();
